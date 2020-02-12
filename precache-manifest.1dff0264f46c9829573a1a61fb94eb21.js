9305415d71a534839f25ebee87479cea7850c5c1
ismhamza <hamza_dsl@hotmail.com>
2020-02-13 00:07:50 +0500

    Updates

---
+++ precache-manifest.1dff0264f46c9829573a1a61fb94eb21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82824fdd779db88b18b6363d45d8897f",
+    "url": "/PrivateSocialVideoNetwork/index.html"
+  },
+  {
+    "revision": "1faa6f2ecf878b78c1cc",
+    "url": "/PrivateSocialVideoNetwork/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "81e7a483347b351d4e6d",
+    "url": "/PrivateSocialVideoNetwork/static/js/2.2a2db8e3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/PrivateSocialVideoNetwork/static/js/2.2a2db8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1faa6f2ecf878b78c1cc",
+    "url": "/PrivateSocialVideoNetwork/static/js/main.60a66689.chunk.js"
+  },
+  {
+    "revision": "54160fc76288e6f2a3e0",
+    "url": "/PrivateSocialVideoNetwork/static/js/runtime-main.6c744ff7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PrivateSocialVideoNetwork/static/media/logo.5d5d9eef.svg"
+  }
+]);
